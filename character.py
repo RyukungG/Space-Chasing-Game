0b9daae4b239c131b111295d9f653aece78e4881
PakornL <48524886+PakornL@users.noreply.github.com>
2021-11-17 13:20:58 +0700

    update docstring

--- character.py
+++ character.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
+from scoreboard import Score
 import random
+import time
 
 class Character:
     def __init__(self, color, speed, x=0, y=0):
@@ -29,20 +31,33 @@ class Player(Character):
     def turn_right(self):
         self.turtle.right(10)
 
+    def quit(self):
+        self.screen.bye()
+
     def control(self):
         self.screen.onkeypress(self.walk_forward, "Up")
         self.screen.onkeypress(self.walk_backwards, "Down")
         self.screen.onkeypress(self.turn_right, "Right")
         self.screen.onkeypress(self.turn_left, "Left")
+        self.screen.onkey(self.quit,"q")
         self.screen.listen()
         self.x, self.y = self.turtle.pos()
 
 class Enemy(Character):
     def __init__(self):
         super().__init__("red", 5, random.randint(-400, 400), random.randint(-400, 400))
+        self.score = Score
 
     def chase(self, player):
         self.turtle.setheading(self.turtle.towards(player.turtle.pos()))
         self.turtle.forward(5)
         self.screen.ontimer(self.chase(player), 1)
+
+    def hit(self, player, player_name):
+        start = time.time()  # start time
+        if player.turtle.pos() == self.turtle.pos():
+            stop = time.time()  # stop time
+            score = stop - start
+            self.score.insert(player_name, int(score))
+            exit()
 
--- screen.py
+++ screen.py
@@ -1,12 +1,24 @@
 from character import Player, Enemy
+from scoreboard import Score
 from turtle import Turtle, Screen
+import time
+
+
 background = Screen()
 background.screensize(600, 600)
 background.bgcolor("black")
+player_name = background.textinput("Player Name", "Enter your name")
 p = Player()
 e = Enemy()
+scoreboard = Score("scoreboard")
+
 e.turtle.setposition(e.x, e.y)
+
+e.hit(p, player_name)
 p.control()
 e.chase(p)
+
 background.mainloop()
+
+
 
