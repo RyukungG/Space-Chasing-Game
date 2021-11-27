2619bcf7da14ea82b3f7bce676ece0bfe9bd9d38
PakornL <48524886+PakornL@users.noreply.github.com>
2021-11-27 19:07:38 +0700

    update docstring

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from screen import RunScreen
+from turtle import title
 
 # create RunScreen object
 background = RunScreen(600, 600, "SPACE_CHASING.gif")
 
 # run game
+title("SPACE CHASING")
 background.menu()
